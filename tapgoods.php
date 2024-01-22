bbe24564536f1fc82443f034227ae682386bda63
Jeremy Benson <jeremy.benson@tapgoods.com>
2024-01-22 17:33:22 -0600

    add main plugin file

---
+++ tapgoods.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+ * Plugin Name:       TapGoods WordPress
+ * Plugin URI:        https://github.com/TapGoods/tapgoods_wp-plugin
+ * Description:       WordPress integration for TapGoods
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            TapGoods
+ * Author URI:        https://www.tapgoods.com/pro/
+ * License:           MIT
+ * Text Domain:       tapgoods-wp
+ * Domain Path:       /languages
+ * 
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2024 TapGoods
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice (including the next paragraph) shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
