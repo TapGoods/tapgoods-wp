1c9b512a22b509639cccc6e3852e8e921d81404b
Jeremy Benson <jeremy.benson@tapgoods.com>
2024-02-22 16:53:12 -0600

    update phpcs linting config and first pass fixes

--- tapgoods-wp/admin/partials/tapgoods-admin-styling.php
+++ tapgoods-wp/admin/partials/tapgoods-admin-styling.php
@@ -1,37 +1,38 @@
-<?php 
+<?php
 
 // Load our filesystem class for file operations
 require_once TAPGOODS_PLUGIN_PATH . 'includes/class-tapgoods-filesystem.php';
 
 $input_submit = 'save_styles';
-$nonce = 'tapgoods-save-custom-css';
-$success = false;
+$nonce        = 'tapgoods-save-custom-css';
+$success      = false;
 $tapgoods_css = Tapgoods_Filesystem::get_file( TAPGOODS_PLUGIN_PATH . '/assets/css/tg-bootstrap.css' );
-$save_text = 'Save';
+$save_text    = 'Save';
 
 ?>
 <h2>Styling</h2>
 <h3>TapGoods Styles</h3>
-<textarea placeholder="" id="tg-css" spellcheck="false"><?php echo esc_textarea($tapgoods_css) ?></textarea>
+<textarea placeholder="" id="tg-css" spellcheck="false"><?php echo esc_textarea( $tapgoods_css ); ?></textarea>
 
 <h3>Custom Styles</h3>
 <?php
 
-if (isset($_POST[$input_submit] )) {
-    $success = Tapgoods_Filesystem::put_file( $input_submit, $_POST['tg-custom-css'], TAPGOODS_UPLOADS . 'custom.css',  $nonce);
-    if (! $success) {
-        var_dump($success); die();
-    }
-    $save_text = ($success) ? 'Saved' : $save_text;
+if ( isset( $_POST[ $input_submit ] ) ) {
+	$success = Tapgoods_Filesystem::put_file( $input_submit, $_POST['tg-custom-css'], TAPGOODS_UPLOADS . 'custom.css', $nonce );
+	if ( ! $success ) {
+		var_dump( $success );
+		die();
+	}
+	$save_text = ( $success ) ? 'Saved' : $save_text;
 }
-$custom_css = Tapgoods_Filesystem::get_file( TAPGOODS_UPLOADS . 'custom.css');
+$custom_css = Tapgoods_Filesystem::get_file( TAPGOODS_UPLOADS . 'custom.css' );
 
 ?>
 <form name="tapgoods_styling" method="post", action="">
-    <?php wp_nonce_field( 'tapgoods-save-custom-css') ?>
-    <textarea placeholder="" name="tg-custom-css" id="tg-custom-css" class="mb-2" spellcheck="false"><?php echo esc_textarea($custom_css) ?></textarea>
-    <hr class="mb-2">
-    <input name="<?php echo $input_submit ?>" type="submit" class="btn btn-primary round" value="<?php echo $save_text ?>">
+	<?php wp_nonce_field( 'tapgoods-save-custom-css' ); ?>
+	<textarea placeholder="" name="tg-custom-css" id="tg-custom-css" class="mb-2" spellcheck="false"><?php echo esc_textarea( $custom_css ); ?></textarea>
+	<hr class="mb-2">
+	<input name="<?php echo $input_submit; ?>" type="submit" class="btn btn-primary round" value="<?php echo $save_text; ?>">
 <form>
 
 <?php
--- tapgoods-wp/includes/class-tapgoods-filesystem.php
+++ tapgoods-wp/includes/class-tapgoods-filesystem.php
@@ -2,64 +2,67 @@
 
 class Tapgoods_Filesystem {
 
-    // static reference to filesytem to prevent doing this more than once
-    private static $filesystem = null;
+	// static reference to filesytem to prevent doing this more than once
+	private static $filesystem = null;
 
 	public static function get_filesystem() {
-		
-        if ( null === self::$filesystem ) {
-            global $wp_filesystem;
-            include_once ABSPATH . 'wp-admin/includes/file.php';
-            WP_Filesystem();
-            self::$filesystem = $wp_filesystem;
-        }
-        return self::$filesystem;
+
+		if ( null === self::$filesystem ) {
+			global $wp_filesystem;
+			include_once ABSPATH . 'wp-admin/includes/file.php';
+			WP_Filesystem();
+			self::$filesystem = $wp_filesystem;
+		}
+		return self::$filesystem;
 	}
 
-    // Uses the WP Filesystem to get the contents of a file
-    public static function get_file($path) {
+	// Uses the WP Filesystem to get the contents of a file
+	public static function get_file( $path ) {
 
-        $filesystem = self::get_filesystem();      
-        return $filesystem->get_contents( $path );
-    }
+		$filesystem = self::get_filesystem();
+		return $filesystem->get_contents( $path );
+	}
 
 
-    public static function put_file($submit, $contents, $filepath, $nonce) {
+	public static function put_file( $submit, $contents, $filepath, $nonce ) {
 
-        // return false if not handling a POST request
-        if (empty($_POST)) return false;
+		// return false if not handling a POST request
+		if ( empty( $_POST ) ) {
+			return false;
+		}
 
-        check_admin_referer($nonce);
+		check_admin_referer( $nonce );
 
-        $method = '';
-        $form_fields = array($submit, $contents);
-        $url = wp_nonce_url('options.php?page=tapgoods', $nonce );
-        
-        // Check if we have credentials to write files
-        if (false === ($creds = request_filesystem_credentials($url, $method, false, false, $form_fields, true) ) ) {
-            return true;
-        }
+		$method      = '';
+		$form_fields = array( $submit, $contents );
+		$url         = wp_nonce_url( 'options.php?page=tapgoods', $nonce );
 
-        if ( ! WP_Filesystem($creds) ) {
-            // our credentials were no good, ask the user for them again
-            request_filesystem_credentials($url, $method, true, false, $form_fields, true);
-            return true;
-        }
+		// Check if we have credentials to write files
+		if ( false === ( $creds = request_filesystem_credentials( $url, $method, false, false, $form_fields, true ) ) ) {
+			return true;
+		}
 
-        $filesystem = self::get_filesystem();
+		if ( ! WP_Filesystem( $creds ) ) {
+			// our credentials were no good, ask the user for them again
+			request_filesystem_credentials( $url, $method, true, false, $form_fields, true );
+			return true;
+		}
 
-        $folder_exists = $filesystem->exists(TAPGOODS_UPLOADS);
-        if ( ! $folder_exists ) {
-            $make_folder = $filesystem->mkdir(TAPGOODS_UPLOADS);
-            echo "couldn't make folder";
-            if( ! $make_folder ) return false;
-        }
-        
-        $success = $filesystem->put_contents( $filepath, $contents, FS_CHMOD_FILE);
-        if( ! $success  ) {
-            return false;
-        }
-        return $success;
-    }
+		$filesystem = self::get_filesystem();
 
+		$folder_exists = $filesystem->exists( TAPGOODS_UPLOADS );
+		if ( ! $folder_exists ) {
+			$make_folder = $filesystem->mkdir( TAPGOODS_UPLOADS );
+			echo "couldn't make folder";
+			if ( ! $make_folder ) {
+				return false;
+			}
+		}
+
+		$success = $filesystem->put_contents( $filepath, $contents, FS_CHMOD_FILE );
+		if ( ! $success ) {
+			return false;
+		}
+		return $success;
+	}
 }
