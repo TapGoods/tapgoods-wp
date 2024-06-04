5519a9697755c64d917d70d0bba8a8b8cfb951f5
Jeremy Benson <jeremy.benson@tapgoods.com>
2024-06-04 11:36:00 -0500

    convert shortcode vars from string to bool

--- tapgoods-wp/public/partials/tg-inventory.php
+++ tapgoods-wp/public/partials/tg-inventory.php
@@ -7,6 +7,8 @@ $tg_inventory_pagination_class = '';
 
 $category = '';
 $tags     = '';
+$show_search  = filter_var( $atts['show_search'], FILTER_VALIDATE_BOOLEAN );
+$show_filters = filter_var( $atts['show_filters'], FILTER_VALIDATE_BOOLEAN );
 
 if ( ! empty( $atts['category'] ) ) {
 	$category = "category={$atts['category']}";
@@ -21,14 +23,14 @@ ob_start();
 
 ?>
 <div class="tapgoods tapgoods-inventory container-fluid">
-	<?php if ( false !== $atts['show_search'] ) : ?>
+	<?php if ( false !== $show_search ) : ?>
 		<?php do_action( 'tg_before_inventory_search' ); ?>
 		<?php echo do_shortcode( '[tapgoods-search]' ) ?>
 		<?php do_action( 'tg_after_inventory_search' ); ?>
 	<?php endif; ?>
 	<div class="container shop">
 		<div class="row align-items-start">
-				<?php if ( false !== $atts['show_filters'] ) : ?>
+				<?php if ( false !== $show_filters ) : ?>
 				[tapgoods-filter]
 				<?php endif; ?>
 			<section class="<?php echo esc_attr( apply_filters( 'tg_inventory_grid_class', $tg_inventory_grid_class ) ); ?>">
